097a1236114f6cc183b27f3b704f68cbf847dec0
neosInPocket <88581498+neosInPocket@users.noreply.github.com>
2024-04-01 19:50:13 +0300

    Initial commit

---
+++ Assets/Scripts/CasualSave/Conserver.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class Conserver : MonoBehaviour
+{
+	[SerializeField] private bool eraseLastData;
+	public static int playerGamePgoress;
+	public static int coinsAmount;
+	public static int storeUpgrade;
+	public static int storeSideUpgrade;
+	public static int soundEffects;
+	public static int musicEffects;
+	public static int casualTrain;
+
+	private void Awake()
+	{
+		if (eraseLastData)
+		{
+			LoadDefaultCandyData();
+			SaveCandyData();
+		}
+		else
+		{
+			LoadCandyData();
+		}
+	}
+	public void LoadDefaultCandyData()
+	{
+		playerGamePgoress = 1;
+		coinsAmount = 200;
+		storeUpgrade = 0;
+		storeSideUpgrade = 0;
+		soundEffects = 1;
+		musicEffects = 1;
+		casualTrain = 1;
+	}
+
+	public static void SaveCandyData()
+	{
+		PlayerPrefs.SetInt("playerGamePgoress", playerGamePgoress);
+		PlayerPrefs.SetInt("coinsAmount", coinsAmount);
+		PlayerPrefs.SetInt("storeUpgrade", storeUpgrade);
+		PlayerPrefs.SetInt("storeSideUpgrade", storeSideUpgrade);
+		PlayerPrefs.SetInt("soundEffects", soundEffects);
+		PlayerPrefs.SetInt("musicEffects", musicEffects);
+		PlayerPrefs.SetInt("casualTrain", casualTrain);
+	}
+
+	public void LoadCandyData()
+	{
+		playerGamePgoress = PlayerPrefs.GetInt("playerGamePgoress", 1);
+		coinsAmount = PlayerPrefs.GetInt("coinsAmount", 0);
+		storeUpgrade = PlayerPrefs.GetInt("storeUpgrade", 0);
+		storeSideUpgrade = PlayerPrefs.GetInt("storeSideUpgrade", 0);
+		soundEffects = PlayerPrefs.GetInt("soundEffects", 1);
+		musicEffects = PlayerPrefs.GetInt("musicEffects", 1);
+		casualTrain = PlayerPrefs.GetInt("casualTrain", 1);
+	}
+}
